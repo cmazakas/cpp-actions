d3611b3c75ab183bb78e2b442dd1758aef5642f1
Christian Mazakas <christian.mazakas@gmail.com>
2024-01-04 12:17:34 -0800

    Use containers for all linux jobs

--- cpp-matrix/index.js
+++ cpp-matrix/index.js
@@ -621,38 +621,31 @@ function generateMatrix(compilerVersions, standards, max_standards, latest_facto
 
             // runs-on / container
             if (compilerName === 'gcc') {
+                entry['runs-on'] = 'ubuntu-latest'
                 if (semver.satisfies(minSubrangeVersion, '>=13')) {
-                  entry['runs-on'] = 'ubuntu-latest'
                   entry['container'] = 'ubuntu:23.04'
-                } else if (semver.satisfies(minSubrangeVersion, '>=9')) {
-                    entry['runs-on'] = 'ubuntu-22.04'
+                } else if (semver.satisfies(minSubrangeVersion, '>=11')) {
+                    entry['container'] = 'ubuntu:22.04'
                 } else if (semver.satisfies(minSubrangeVersion, '>=7')) {
                     entry['runs-on'] = 'ubuntu-20.04'
                 } else {
-                    entry['runs-on'] = 'ubuntu-22.04'
                     entry['container'] = 'ubuntu:18.04'
                 }
             } else if (compilerName === 'clang') {
+                entry['runs-on'] = 'ubuntu-latest'
                 if (semver.satisfies(minSubrangeVersion, '>=17')) {
-                  entry['runs-on'] = 'ubuntu-latest'
                   entry['container'] = 'ubuntu:23.10'
                 } else if (semver.satisfies(minSubrangeVersion, '>=16')) {
-                  entry['runs-on'] = 'ubuntu-latest'
                   entry['container'] = 'ubuntu:23.04'
-                } else if (semver.satisfies(minSubrangeVersion, '>=15')) {
-                    entry['runs-on'] = 'ubuntu-22.04'
-                } else if (semver.satisfies(minSubrangeVersion, '>=12')) {
-                    // Clang >=12 <15 require a container to isolate
-                    // incompatible libstdc++ versions
-                    entry['runs-on'] = 'ubuntu-22.04'
+                } else if (semver.satisfies(minSubrangeVersion, '>=11')) {
                     entry['container'] = 'ubuntu:22.04'
                 } else if (semver.satisfies(minSubrangeVersion, '>=6')) {
-                    entry['runs-on'] = 'ubuntu-20.04'
+                    // Clang >=12 <15 require a container to isolate
+                    // incompatible libstdc++ versions
+                    entry['container'] = 'ubuntu:20.04'
                 } else if (semver.satisfies(minSubrangeVersion, '>=3.9')) {
-                    entry['runs-on'] = 'ubuntu-22.04'
                     entry['container'] = 'ubuntu:18.04'
                 } else {
-                    entry['runs-on'] = 'ubuntu-22.04'
                     entry['container'] = 'ubuntu:16.04'
                 }
             } else if (compilerName === 'msvc') {
